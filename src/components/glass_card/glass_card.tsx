92014bcd4e70f329a0ab8bb6075cabeeeece5ec4
JheisonTavares <133679772+JheisonTavares@users.noreply.github.com>
2025-05-15 16:33:56 -0400

    [feat]refactoret fouders

--- src/components/glass_card/glass_card.tsx
+++ src/components/glass_card/glass_card.tsx
@@ -4,6 +4,7 @@ export default function GlassCard() {
     return (
         <div className="glass_card">
             <img src="./assets/images/macacos_cocos.jpg" alt="" />
+            <div className="glass_card_hiden_text">Bacaco que banana</div>
         </div>
     );
 }
